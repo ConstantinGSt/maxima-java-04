4571cf4668132a0743501731955ac687799e2c72
Constantin St <dinscalin.ks@gmail.com>
2022-11-09 01:16:21 +0700

    repait loombok

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import lombok.Setter;
+
 
 /**
  * Hello world!
@@ -10,9 +10,11 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 
-	Train tr1 = new Train("tutu", 15, 35, 10);
-	Train tr2 = new Train("rr", 5, 45, 15);
-	@Setter tr1.id 
+	Bus tr1 = new Bus("tutu", 15, 35, 10);
+	Bus tr2 = new Bus("rr", 5, 45, 15);
+	
+	System.out.println(tr1);
+	System.out.println(tr2);
 	}
 	
 }
